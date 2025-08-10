262e18fe52464ef18ee417c074bcf8206ac398b4
satoshi-sh <satoss1108@gmail.com>
2025-08-10 15:43:38 -0500

    Fixed video card responsiblity

--- resources/views/components/cards/video.blade.php
+++ resources/views/components/cards/video.blade.php
@@ -2,7 +2,7 @@
 
 @php
     $thubmnailUrl = 'https://i.ytimg.com/vi/' . $video['id'] . '/hqdefault.jpg';
-    $cardClasses = "flex flex-col shrink-0 gap-2 bg-white/20 p-3 sm:w-96 w-60 h-75 rounded-md";
+    $cardClasses = "flex flex-col shrink-0 gap-2 bg-white/20 p-3 sm:w-96 w-60 sm:h-75 h-86  rounded-md";
 //    if ($video['is_featured']) {
 //        $cardClasses .= " border-1 border-green-300"; // highlight
 //    }
@@ -14,7 +14,7 @@
         <h4 class="text-xl group-hover:text-blue-300 transition-colors duration-500">{{$video->title}}</h4>
         <img class="w-full h-47 object-cover mt-2 rounded-xl" src="{{$thubmnailUrl}}" alt="video thumnail">
     </a>
-    <div class="flex gap-2 grow flex-start items-center">
+    <div class="flex gap-2 grow flex-start flex-wrap items-center">
         @foreach($video->categories as $category)
         <x-tag href="{{'/categories/' . $category->id}}" size="small">{{$category->name}}</x-tag>
         @endforeach
