a0d08a1c821ad7fe41ad8ce52b9ff3b9bb44de24
satoshi-sh <satoss1108@gmail.com>
2025-08-10 11:42:43 -0500

    Changed the styling of categories/show page

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <x-page-heading># {{$category->name}}</x-page-heading>
-    <article class="scrollbar-hide mt-5 flex gap-4 overflow-x-auto whitespace-nowrap ">
+    <article class=" mt-5 mb-5 flex flex-wrap gap-5 justify-center mx-auto">
         @foreach($videos as $video)
             <x-cards.video :video="$video"/>
         @endforeach
