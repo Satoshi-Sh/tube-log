75ba489f08e53db0090dfec0bf7dcd4dd0f052be
satoshi-sh <satoss1108@gmail.com>
2025-08-11 10:05:58 -0500

    Updated README.md and seed to create an user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => env('SEED_ADMIN_NAME'),
+            'email' => env('SEED_ADMIN_EMAIL'),
+            'password' => bcrypt(env('SEED_ADMIN_PASSWORD')),
         ]);
     }
 }
